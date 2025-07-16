1d5c6021fe3dd474a111cfd8e173a52ad85f5a38
Hasitha Mihiranga <119967987+hm7mihiranga@users.noreply.github.com>
2025-07-16 13:38:25 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Information_Retreval_System',
+    version='0.1',
+    author='Hasitha Mihiranga',
+    description='A system for information retrieval',
+    author_email='hm.mihi7@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
